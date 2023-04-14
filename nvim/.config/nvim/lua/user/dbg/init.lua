53409716cd32c753a8099346266fd9ee0da53935
Jordan <jordan.geltchrobb@monarcglobal.com>
2023-04-14 15:31:34 +1000

    chore: update mason-nvim-dap handlers setup function

--- nvim/.config/nvim/lua/user/dbg/init.lua
+++ nvim/.config/nvim/lua/user/dbg/init.lua
@@ -1,11 +1,19 @@
 require("mason-nvim-dap").setup({
-  ensure_installed = { "typescript", "node" },
-  automatic_setup = true
+	ensure_installed = { "typescript", "node" },
+	automatic_setup = true,
+	handlers = {
+		function(config)
+			-- all sources with no handler get passed here
+
+			-- Keep original functionality
+			require('mason-nvim-dap').default_setup(config)
+		end,
+	}
 })
 
 
 
-require("mason-nvim-dap").setup_handlers()
+--[[ require("mason-nvim-dap").setup_handlers() ]]
 require('telescope').load_extension('dap')
 
 -- DAP-UI --
@@ -14,4 +22,3 @@ require("dapui").setup()
 
 -- nvim-dap-virtual-text. Show virtual text for current frame
 vim.g.dap_virtual_text = true
-
