ea41bed48de0f2559c285659bd352b57526d8fbb
Jordan <jdj.dev90@gmail.com>
2024-06-29 15:31:15 +1000

    update gitsigns highlights

--- nvim/.config/nvim/lua/user/gitsigns.lua
+++ nvim/.config/nvim/lua/user/gitsigns.lua
@@ -3,17 +3,52 @@ if not status_ok then
   return
 end
 
-gitsigns.setup {
+vim.api.nvim_set_hl(0, "GitSignsAdd", { link = "GitSignsAdd" })
+vim.api.nvim_set_hl(0, "GitSignsChange", { link = "GitSignsChange" })
+vim.api.nvim_set_hl(0, "GitSignsDelete", { link = "GitSignsDelete" })
+vim.api.nvim_set_hl(0, "GitSignsAdd", { link = "GitSignsAdd" })
+vim.api.nvim_set_hl(0, "GitSignsAdd", { link = "GitSignsAdd" })
+
+gitsigns.setup({
+
+  -- Define highlights
+  on_attach = function()
+    -- Add highlights
+    vim.api.nvim_set_hl(0, "GitSignsAdd", { link = "DiffAdd" })
+    vim.api.nvim_set_hl(0, "GitSignsAddLn", { link = "DiffAdd" })
+    vim.api.nvim_set_hl(0, "GitSignsAddNr", { link = "DiffAdd" })
+
+    -- Change highlights
+    vim.api.nvim_set_hl(0, "GitSignsChange", { link = "DiffChange" })
+    vim.api.nvim_set_hl(0, "GitSignsChangeLn", { link = "DiffChange" })
+    vim.api.nvim_set_hl(0, "GitSignsChangeNr", { link = "DiffChange" })
+
+    -- Change delete highlights
+    vim.api.nvim_set_hl(0, "GitSignsChangedelete", { link = "DiffChange" })
+    vim.api.nvim_set_hl(0, "GitSignsChangedeleteLn", { link = "DiffChange" })
+    vim.api.nvim_set_hl(0, "GitSignsChangedeleteNr", { link = "DiffChange" })
+
+    -- Delete highlights
+    vim.api.nvim_set_hl(0, "GitSignsDelete", { link = "DiffDelete" })
+    vim.api.nvim_set_hl(0, "GitSignsDeleteLn", { link = "DiffDelete" })
+    vim.api.nvim_set_hl(0, "GitSignsDeleteNr", { link = "DiffDelete" })
+
+    -- Top delete highlights
+    vim.api.nvim_set_hl(0, "GitSignsTopdelete", { link = "DiffDelete" })
+    vim.api.nvim_set_hl(0, "GitSignsTopdeleteLn", { link = "DiffDelete" })
+    vim.api.nvim_set_hl(0, "GitSignsTopdeleteNr", { link = "DiffDelete" })
+  end,
+
   signs = {
-    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    add = { text = "▎" },
+    change = { text = "▎" },
+    delete = { text = "契" },
+    topdelete = { text = "契" },
+    changedelete = { text = "▎" },
   },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-  numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-  linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+  numhl = false,    -- Toggle with `:Gitsigns toggle_numhl`
+  linehl = false,   -- Toggle with `:Gitsigns toggle_linehl`
   word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
   watch_gitdir = {
     interval = 1000,
@@ -26,9 +61,6 @@ gitsigns.setup {
     virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
     delay = 1000,
     ignore_whitespace = false,
-  },
-  current_line_blame_formatter_opts = {
-    relative_time = false,
   },
   sign_priority = 6,
   update_debounce = 100,
@@ -45,4 +77,4 @@ gitsigns.setup {
   yadm = {
     enable = false,
   },
-}
+})
